2cebaf1fffd5e243ad24d93095ad34c460c85682
benjammin12 <benxerri@gmail.com>
2017-01-22 13:31:04 -0500

    Initial commit

---
+++ Assets/Assets/Scripts/IntroMusic.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroMusic : MonoBehaviour {
+
+	static IntroMusic instance = null;
+
+	void Awake() {
+		if (instance != null) {
+			Destroy (gameObject);
+			print ("Music Stopped");
+		} else {
+            instance = this;
+			GameObject.DontDestroyOnLoad (gameObject); // Game Object in this case is the Intro Music component we created that holds music
+		}	
+	}
+}
---
+++ Assets/Assets/Scripts/ShipControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipControl : MonoBehaviour {
+
+public float speedX  = 8.0f;
+public float speedY = 50.0f;
+
+	void Update () {
+
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * speedX; //movement left and right
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * speedY; //movement up and down
+		 transform.Translate(0, x, z);
+
+        if (transform.position.y > 5.4f) //if we move our ship greater than y position 5.4, move it back to 5.4
+        	transform.position = new Vector3( transform.position.x , 5.4f); //keep x the same
+        else if( transform.position.y < -5.4f) 
+        	transform.position = new Vector3 (transform.position.x , -5.4f);
+        else if( transform.position.x < -12.45f)
+        	transform.position = new Vector3 (-12.45f , transform.position.y); 
+        
+     }
+}
